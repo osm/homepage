72b423debd585f88bb4d7943f795e7ea24c6377d
Oscar Linderholm <osm@recv.se>
2022-11-29 17:29:39 +0100

    Be dynamic

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
 	"flag"
+	"io/fs"
+	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 //go:embed index.html
 var index []byte
 
-//go:embed favicon.png
-var favicon []byte
+//go:embed static
+var staticFS embed.FS
 
 func main() {
 	port := flag.String("p", "4001", "listen port")
 	flag.Parse()
 
-	http.HandleFunc("/favicon.png", func(w http.ResponseWriter, req *http.Request) {
-		w.Write(favicon)
+	files, _ := fs.ReadDir(staticFS, "static")
+	for _, f := range files {
+		n := f.Name()
+		d, _ := staticFS.ReadFile(filepath.Join("static", n))
 
-	})
+		http.HandleFunc("/"+n, func(w http.ResponseWriter, req *http.Request) {
+			ip := req.Header.Get("X-Real-IP")
+			if ip == "" {
+				ip = req.RemoteAddr[0:strings.LastIndex(req.RemoteAddr, ":")]
+			}
+			log.Printf("%s downloaded from %s", n, ip)
+			w.Write(d)
+
+		})
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Write(index)
 	})
